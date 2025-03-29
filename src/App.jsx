ed1405ac9bf8b949381882cf781d67af60db6adb
chintan dankhara <chintandankhara9@gmail.com>
2025-03-29 08:26:56 +0530

    f

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import {  Route, Routes } from "react-router-dom"
-import React, { useEffect, useLayoutEffect, useState } from "react"
 import { Home } from "./pages/Home"
 import { Allscheme } from "./pages/Allscheme"
 import { Signin} from "./components/Signin.jsx"
 import { Signup } from "./components/Signup.jsx"
 import { As } from "./components/As.jsx"
 function App() {
-    // const [get, set] = useState(false);
     
-    // useLayoutEffect(()=>{
-    //   const g = document.cookie.split("=")[1];
-    //   if(g=="chintan"){
-    //     set(true)
-    //   }
-    // },[set])
-
   return (
     <>
       <Routes>
